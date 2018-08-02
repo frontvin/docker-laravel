7e638fc09d9289cf95e8a383ef43985d641b6501
Vadim <vbombik@devtoday.com>
2018-08-02 16:50:53 +0300

    added configuration to .env and dataabase.php

--- html/blog/config/database.php
+++ html/blog/config/database.php
@@ -35,11 +35,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'database'),
+            'host' => env('DB_HOST', '172.25.0.3'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravel_docker'),
+            'username' => env('DB_USERNAME', 'site'),
+            'password' => env('DB_PASSWORD', 'password'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
