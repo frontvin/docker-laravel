4a7e7ff5d3d133deea897d2726d2ad0e4f1ecc4c
Vadim <vbombik@devtoday.com>
2018-08-08 14:17:32 +0300

    Finished laravel landing project

--- html/blog/resources/views/admin.blade.php
+++ html/blog/resources/views/admin.blade.php
@@ -6,13 +6,24 @@
     <link rel="shortcut icon" href="http://chocolat.nois3.net/app/themes/chocolat/img/favicon.jpg">
 </head>
 <body>
-    <div>
-        @foreach ($orders as $order)
 
-        <p> Name : {{ $order->name }} </p> 
-        <p> Email: {{ $order->email }} </p>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($orders as $order)
+    <tr>
+      <td>{{ $order->name }}</td>
+      <td>{{ $order->email }}</td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 
-        @endforeach
-    </div>
-</div>
+</body>
+</html>
 
